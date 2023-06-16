4f67e51c54c7f8fbed22e9bd714173b3b4f8d780
sebarck <sebasmonti206@gmail.com>
2023-06-15 21:02:14 -0300

    Initial commit with all the required files.

---
+++ README.md
@@ -0,0 +1,43 @@
+# :rocket: Project Name
+
+CoderHouse project to pull BCRA information from this API https://estadisticasbcra.com/ and store it in a Database. In this case we use Amazon Redshift.
+
+## :memo: Table of Contents
+
+- [:rocket: Project Name](#rocket-project-name)
+  - [:memo: Table of Contents](#memo-table-of-contents)
+  - [:package: Installation](#package-installation)
+  - [:wrench: Configuration](#wrench-configuration)
+  - [:computer: Usage](#computer-usage)
+
+## :package: Installation 
+
+1. Clone the repository
+
+2. Install the required dependencies:
+```bash
+pip install -r requirements.txt
+```
+## :wrench: Configuration
+
+To configure the application, create a .env file in the root directory and provide the following environment variables:
+
+```makefile
+REDSHIFT_HOST=your_redshift_host
+REDSHIFT_PORT=your_redshift_port
+REDSHIFT_DBNAME=your_redshift_dbname
+REDSHIFT_USER=your_redshift_user
+REDSHIFT_PASSWORD=your_redshift_password
+OPTIONS=your_redshift_options
+BEARER_TOKEN=your_bearer_token
+```
+If you don't have them, please contact smonti@eurekalabs.io.
+
+## :computer: Usage
+
+You need to only run the script:
+
+```bash
+python main.py
+```
+Follow the prompts to follow the different execution results and keep track of the errors if any
---
+++ main.py
@@ -0,0 +1,82 @@
+import requests
+import pandas as pd
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+# If you don't have them, please ask to smonti@eurekalabs.io
+load_dotenv()
+
+# API endpoint URL to retrieve UVA index historical values
+api_url = "https://api.estadisticasbcra.com/uva"
+
+headers = {
+    "Authorization": f"Bearer {os.environ.get('BEARER_TOKEN')}"
+}
+
+response = requests.get(api_url, headers=headers)
+data = response.json()
+
+# Create a dictionary to store the data
+historical_data = {}
+
+# Store the API response in the dictionary
+for entry in data:
+    date = entry['d']
+    value = entry['v']
+    historical_data[date] = value
+
+print(historical_data['2023-06-15'])
+
+# Create the Panda DataFrame with the dictionary data
+df = pd.DataFrame.from_dict(historical_data, orient='index', columns=['value'])
+
+try:
+    conn = psycopg2.connect(
+        host=os.environ.get('REDSHIFT_HOST'),
+        port=os.environ.get('REDSHIFT_PORT'),
+        dbname=os.environ.get('REDSHIFT_DBNAME'),
+        user=os.environ.get('REDSHIFT_USER'),
+        password=os.environ.get('REDSHIFT_PASSWORD'),
+        options=os.environ.get('OPTIONS', '')
+    )
+    print("Connected to the database successfully.")
+except Exception as e:
+    print("Error connecting to the database:", e)
+
+# Create the Redshift table IF NOT EXISTS. If exists will success anyways, consider that
+try: 
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS uva_historical_values (date DATE PRIMARY KEY, value FLOAT)")
+    print("Table created successfully or already exists.")
+except Exception as e:
+    print("Error creating the database. Details: ", e)
+
+# The insert will be made in batches so the performance is increased.
+batch_size = 1000
+
+try:
+    # Truncate the table so every time the script runs, we ensure to refresh all the historical data
+    cursor.execute("TRUNCATE TABLE uva_historical_values;")
+    print("Table truncated successfully.")
+
+    # Data insertion in batches
+    for i in range(0, len(df), batch_size):
+        batch = df.iloc[i:i + batch_size]
+        insert_values = ", ".join(
+            "(TO_DATE('%s', 'YYYY-MM-DD'), %s)" % (index, row['value']) for index, row in batch.iterrows()
+        )
+        insert_query = "INSERT INTO uva_historical_values (date, value) VALUES " + insert_values + ";"
+        cursor.execute(insert_query)
+
+    conn.commit()
+    print("Data inserted successfully!")
+
+except Exception as e:
+    print("Error:", e)
+
+finally:
+    cursor.close()
+    conn.close()
+    print("Closed connection. Bye!")
